fa9bdf5e55be4c84c85c5e7924dbe3cda9059a99
faturrizqi22 <rizqi14th@gmail.com>
2020-12-14 19:22:16 +0700

    Add project files.

---
+++ CouponWithoutOCP/Coupon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithoutOCP
+{
+    class Coupon
+    {
+        public double discNominal = 0;
+        public double disPercentage = 0;
+        public double disCashback = 0;
+        public double priceNett(double originPrice)
+        {
+            double net = 0;
+            if (discNominal == 0 && disPercentage > 0)
+            {
+                net = (100 - disPercentage) * originPrice / 100;
+            }
+            else if(discNominal > 0 && disPercentage == 0)
+            {
+                net = originPrice - discNominal;
+            }
+            else if(discNominal > 0 && disPercentage > 0)
+            {
+                net = originPrice - discNominal;
+            }
+            return net;
+        }
+    }
+}
